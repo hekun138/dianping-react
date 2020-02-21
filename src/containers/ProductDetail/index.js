280252bfb396a7582d0f97efb752d0a5d4446b96
Hogan <418730859@qq.com>
2020-02-21 23:34:40 +0800

    设计reducer

--- src/containers/ProductDetail/index.js
+++ src/containers/ProductDetail/index.js
@@ -3,15 +3,24 @@ import ProductOverview from "./components/ProductOverview";
 import ShopInfo from "./components/ShopInfo";
 import Detail from "./components/Detail";
 import Remark from "./components/Remark";
+import BuyButton from "./components/BuyButton";
+import Header from "../../components/Header";
 
 class ProductDetail extends Component {
+
+  handleBack = () => {
+    
+  }
+
   render() {
     return (
       <div>
+        <Header title="团购详情" onBack={this.handleBack} grey/>
         <ProductOverview />
         <ShopInfo />
         <Detail />
         <Remark />
+        <BuyButton />
       </div>
     );
   }
--- src/redux/modules/detail.js
+++ src/redux/modules/detail.js
@@ -1,5 +1,128 @@
-const reducer = (state = {}, action) => {
-  return state;
+import { combineReducers } from "redux";
+import { FETCH_DATA } from "../middleware/api";
+import { schema as shopSchema, getShopById } from "./entities/shops";
+import { schema as productSchems, getProductDetail } from "./entities/products";
+
+export const types = {
+  //获取产品详情
+  FETCH_PRODUCT_DETAIL_REQUEST: 'DETAIL/FETCH_PRODUCT_DETAIL_REQUEST',
+  FETCH_PRODUCT_DETAIL_SUCCESS: 'DETAIL/FETCH_PRODUCT_DETAIL_SUCCESS',
+  FTECH_PRODUCT_DETAIL_FAILURE: 'DETAIL/FTECH_PRODUCT_DETAIL_FAILURE',
+  //获取关联店铺信息
+  FETCH_SHOP_REQUEST: 'DETAIL/FETCH_SHOP_REQUEST',
+  FETCH_SHOP_SUCCESS: 'DETAIL/FETCH_SHOP_SUCCESS',
+  FETCH_SHOP_FAILURE: 'DETAIL/FETCH_SHOP_FAILURE'
 }
+
+const initialState = {
+  product: {
+    isFetching: false,
+    id: null
+  },
+  relatedShop: {
+    isFetching: false,
+    id: null
+  }
+}
+
+export const actions = {
+  //获取商品详情
+  loadProductDetail: id => {
+    return (dispatch, getState) => {
+      const product = getProductDetail(getState(), id);
+      if(product) {
+        return dispatch(fetchProductDetailSuccess(id));
+      }
+      const endpoint = url.getProductDetail(id);
+      return dispatch(fetchProductDetail(endpoint, id));
+    }
+  },
+  //获取店铺信息
+  loadShopById: id => {
+    return (dispatch, getState) => {
+      const product = getShopById(getState(), id);
+      if(product) {
+        return dispatch(fetchShopSuccess(id));
+      }
+      const endpoint = url.getShopById(id);
+      return dispatch(fetchShopById(endpoint, id));
+    }
+  }
+}
+//action creators
+const fetchProductDetail = (endpoint, id) => ({
+  [FETCH_DATA]: {
+    types:[
+      types.FETCH_PRODUCT_DETAIL_REQUEST,
+      types.FETCH_PRODUCT_DETAIL_SUCCESS,
+      types.FETCH_PRODUCT_DETAIL_REQUEST,
+    ],
+    endpoint,
+    schema: shopSchema
+  }
+});
+
+const fetchProductDetailSuccess = (id) => ({
+  type: types.FETCH_PRODUCT_DETAIL_SUCCESS,
+  id
+});
+
+const fetchShopById = (id) => ({
+  [FETCH_DATA]: {
+    types:[
+      types.FETCH_SHOP_REQUEST,
+      types.FETCH_SHOP_SUCCESS,
+      types.FETCH_SHOP_FAILURE,
+    ],
+    endpoint,
+    schema: productSchems
+  },
+  id
+});
+
+const fetchShopSuccess = (id) => ({
+  type: types.FETCH_SHOP_SUCCESS,
+  id
+});
+//商品详情reducer
+const product = (state = initialState.product, action) => {
+  switch (action.type) {
+    case types.FETCH_PRODUCT_DETAIL_REQUEST:
+      return { ...state, isFetching: true };
+    case types.FETCH_PRODUCT_DETAIL_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        id: action.id
+      }  
+    case types.FTECH_PRODUCT_DETAIL_FAILURE:
+      return { ...state, isFetching: false }  
+    default:
+      return state;  
+  }
+} 
+//店铺详情reducer
+const relatedShop = (state = initialState.relatedShop, action) => {
+  switch (action.type) {
+    case types.FETCH_SHOP_REQUEST:
+      return { ...state, isFetching: true };
+    case types.FETCH_SHOP_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        id: action.id
+      }  
+    case types.FETCH_SHOP_FAILURE:
+      return { ...state, isFetching: false }  
+    default:
+      return state;  
+  }
+}
+
+//reducer
+const reducer = combineReducers({
+  product,
+  relatedShop
+})
 
 export default reducer;
