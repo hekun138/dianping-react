ad4d6afddc949164cf0710c3bea205af2af87a66
Hogan <418730859@qq.com>
2020-02-23 21:43:18 +0800

    设计搜索页action

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -3,13 +3,15 @@ import entities from "./entities";
 import home from "./home";
 import detail from "./detail";
 import app from "./app";
+import search from "./search";
 
 //合并成根reducer
 const rootReducer = combineReducers({
   entities,
   home,
   detail,
-  app
+  app,
+  search
 });
 
 export default rootReducer;
--- src/redux/modules/search.js
+++ src/redux/modules/search.js
@@ -1,3 +1,24 @@
+import url from "../../utils/url";
+import { FETCH_DATA } from "../middleware/api";
+import { schema as keywordSchame } from "../modules/entities/keywords";
+
+export const types = {
+  //获取热门关键词
+  FETCH_POPULAR_KEYWORDS_REQUEST: "SEARCH/FETCH_POPULAR_KEYWORDS_REQUEST",
+  FETCH_POPULAR_KEYWORDS_SUCCESS: "SEARCH/FETCH_POPULAR_KEYWORDS_SUCCESS",
+  FETCH_POPULAR_KEYWORDS_FAILURE: "SEARCH/FETCH_POPULAR_KEYWORDS_FAILURE",
+  //根据输入的文本获取相关关键词
+  FETCH_RELATED_KEYWORDS_REQUEST: "SEARCH/FETCH_RELATED_KEYWORDS_REQUEST",
+  FETCH_RELATED_KEYWORDS_SUCCESS: "SEARCH/FETCH_RELATED_KEYWORDS_SUCCESS",
+  FETCH_RELATED_KEYWORDS_FAILURE: "SEARCH/FETCH_RELATED_KEYWORDS_FAILURE",
+  //设置当前输入
+  SET_INPUT_TEXT: "SEARCH/SET_INPUT_TEXT",
+  CLEAR_INPUT_TEXT: "SEARCH/CLEAR_INPUT_TEXT",
+  //历史查询记录
+  ADD_HISTORY_KEYWORD: "SEARCH/ADD_HISTORY_KEYWORD",
+  CLEAR_HISTORY_KEYWORDS: "SEARCH/CLEAR_HISTORY_KEYWORDS"
+};
+
 const initialState = {
   inputText: "",
   popularKeywords: {
@@ -17,3 +38,68 @@ const initialState = {
   //保存关键词id
   historyKeywords: []
 };
+
+export const actions = {
+  //获取热门关键词
+  loadPopularKeywords: () => {
+    return (dispatch, getState) => {
+      const { ids } = getState().search.popularKeywords;
+      if (ids.length > 0) {
+        return null;
+      }
+      const endpoint = url.getPopularKeywords();
+      return dispatch(fetchPopularKeywords(endpoint));
+    };
+  },
+  //根据输入获取相关关键词
+  loadRelatedKeywords: text => {
+    return (dispatch, getState) => {
+      const { relatedKeywords } = getState().search;
+      if (relatedKeywords[text]) {
+        return null;
+      }
+      const endpoint = url.getRelatedKeywords(text);
+      return dispatch(fetchRelatedKeywords(text, endpoint));
+    };
+  },
+  //
+  setInputText: text => ({
+    type: types.SET_INPUT_TEXT,
+    text
+  }),
+  clearInputText: () => ({
+    type: types.CLEAR_INPUT_TEXT
+  }),
+  addHistoryKeywords: keywordId => ({
+    type: types.ADD_HISTORY_KEYWORD,
+    text: keywordId
+  }),
+  clearHistoryKeywords: () => ({
+    type: types.CLEAR_HISTORY_KEYWORDS
+  })
+};
+
+const fetchPopularKeywords = endpoint => ({
+  [FETCH_DATA]: {
+    types: [
+      types.FETCH_POPULAR_KEYWORDS_REQUEST,
+      types.FETCH_POPULAR_KEYWORDS_SUCCESS,
+      types.FETCH_POPULAR_KEYWORDS_FAILURE
+    ],
+    endpoint,
+    schema: keywordSchame
+  }
+});
+
+const fetchRelatedKeywords = (text, endpoint) => ({
+  [FETCH_DATA]: {
+    types: [
+      types.FETCH_RELATED_KEYWORDS_REQUEST,
+      types.FETCH_RELATED_KEYWORDS_SUCCESS,
+      types.FETCH_RELATED_KEYWORDS_FAILURE
+    ],
+    endpoint,
+    schema: keywordSchame
+  },
+  text
+});
